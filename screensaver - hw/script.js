81f27a1b92f76096eb7efbb784e81c1300910288
Euniece Harris <euniharris@icloud.com>
2020-09-24 18:05:08 -0400

    Overdue Stuff Bro

---
+++ screensaver - hw/script.js
@@ -0,0 +1,17 @@
+// const monthName = ['Januari', 'Februari', 'Mars', 'April', 'Mai', 'Juni', 'Juli', 'Augusti', 'September', 'October', 'November', 'December']
+// const dayName = ['Måndag', 'Tisdag', 'Onsdag', 'Torsdag', 'Fredag', 'Lordag', 'Söndag']
+
+// var t = new Date();
+// var dayNumber = t.getDay();
+// var month = t.getMonth();
+// var year = t.getFullYear();
+
+// document.getElementsByClassName('item-1').innerHTML= t; 
+
+$('.banner-1').marquee({
+    duration: 10000,
+});
+$('.banner-3').marquee({
+    direction: 'right',
+    duration: 10000,
+});
